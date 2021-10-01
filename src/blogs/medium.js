efec3b337c1264d2058dd6e848bbf12b9ec5cd0b
Restyled.io <commits@restyled.io>
2021-10-01 10:29:44 +0000

    Restyled by prettier

--- src/blogs/medium.js
+++ src/blogs/medium.js
@@ -1,26 +1,26 @@
 const axios = require("axios");
-const {convert} = require("html-to-text");
+const { convert } = require("html-to-text");
 
 let userName = null;
 
-const setUsername = (username) => { userName = username; };
+const setUsername = (username) => {
+  userName = username;
+};
 
 const getEndpoint = async () =>
-    `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${
-        userName}`;
+  `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${userName}`;
 
 const getBlogs = async () => {
   const endpoint = await getEndpoint();
-  const {data} = await axios.get(endpoint);
-  return data.items.map(
-      (article) => ({
-        title : article.title,
-        url : article.link,
-        cover_image : article.thumbnail,
-        description : convert(article.description, {wordwrap : 130}),
-        tags : article.categories,
-        published : Date(article.pubDate),
-      }));
+  const { data } = await axios.get(endpoint);
+  return data.items.map((article) => ({
+    title: article.title,
+    url: article.link,
+    cover_image: article.thumbnail,
+    description: convert(article.description, { wordwrap: 130 }),
+    tags: article.categories,
+    published: Date(article.pubDate),
+  }));
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-const blogs = require('./blogs');
+const blogs = require("./blogs");
 
-const config = ({
-  devAPiKey = null,
-  mediumUsername = null,
-}) => {
+const config = ({ devAPiKey = null, mediumUsername = null }) => {
   blogs.dev.setApiKey(devAPiKey);
   blogs.medium.setUsername(mediumUsername);
 };
@@ -11,9 +8,13 @@ const config = ({
 const getBlogs = async () => {
   let posts = [];
   posts = posts.concat(
-      await blogs.dev.getBlogs().map((blog) => ({...blog, platform : 'dev'})));
-  posts = posts.concat(await blogs.medium.getBlogs().map(
-      (blog) => ({...blog, platform : 'medium'})));
+    await blogs.dev.getBlogs().map((blog) => ({ ...blog, platform: "dev" }))
+  );
+  posts = posts.concat(
+    await blogs.medium
+      .getBlogs()
+      .map((blog) => ({ ...blog, platform: "medium" }))
+  );
   return posts;
 };
 
